9f2441f9b3ad205725f90ac4e6a45c7bbfab023b
Jesse Shawl <jesse@jshawl.com>
2015-01-05 21:17:41 -0500

    add update and destroy

--- app/controllers/smells_controller.rb
+++ app/controllers/smells_controller.rb
@@ -1,11 +1,36 @@
 class SmellsController < ApplicationController
+
   def index
     @smells = Smell.all
   end
+
   def show
     @smell = Smell.find( params[:id] )
   end
+
   def edit
     @smell = Smell.find( params[:id] )
   end
+
+  def update
+    @smell = Smell.find( params[:id] )
+    if @smell.update( smell_params )
+      redirect_to @smell
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @smell = Smell.find( params[:id] )
+    @smell.destroy
+    redirect_to smells_path
+  end
+
+  private
+
+  def smell_params
+    params.require(:smell).permit(:title, :image_url, :location_url)
+  end
+
 end

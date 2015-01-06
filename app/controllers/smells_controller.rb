567ed37e35131e58fe06517e06a09915b3018cca
Jesse Shawl <jesse@jshawl.com>
2015-01-05 21:27:34 -0500

    add crud functionality

--- app/controllers/smells_controller.rb
+++ app/controllers/smells_controller.rb
@@ -12,6 +12,19 @@ class SmellsController < ApplicationController
     @smell = Smell.find( params[:id] )
   end
 
+  def new
+    @smell = Smell.new
+  end
+
+  def create
+    @smell = Smell.new smell_params
+    if @smell.save
+     redirect_to @smell
+    else
+      render :new
+    end
+  end
+
   def update
     @smell = Smell.find( params[:id] )
     if @smell.update( smell_params )
